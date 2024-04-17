8c423477a5ed13bf08f4f66fca065d55c86cfaf3
MrsSalman19 <786saimasos@gmail.com>
2024-04-17 07:04:57 -0700

    push my cli number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Plz guess a Number",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations!You guessed a right Number.");
+}
+else {
+    console.log("You guessed a wrong number.");
+}
